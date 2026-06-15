9f00945358bbd1b3f6b4ec49d0b98e8977778ea6
Ferp <fisck2016@gmail.com>
2026-06-15 01:49:34 -0300

    Arrumando o caminho da mensagem. 	modified:   Pasta/algo.py 	new file:   Pasta/armazem.txt

--- Pasta/algo.py
+++ Pasta/algo.py
@@ -1,5 +1,8 @@
 import requests 
+import os
 
+pasta_do_script = os.path.dirname(os.path.abspath(__file__))
+caminho_completo_txt = os.path.join(pasta_do_script, 'armazem.txt')
 url = 'https://stoic.tekloon.net/stoic-quote'
 
 response = requests.get(url)
@@ -7,6 +10,6 @@ response.raise_for_status()  # Verifica se a requisição foi bem-sucedida
 data = response.json()  # Converte a resposta para JSON
 quote = str(data['data']['quote'])  # Obtém a citação do JSON
 print(f"Citação estoica: {quote}") 
-with open('armazem.txt', 'a', encoding='utf-8') as file:
+with open(caminho_completo_txt, 'a', encoding='utf-8') as file:
     file.write(f'{quote}' + '\n')
     print('Citação salva no arquivo armazem.txt')
